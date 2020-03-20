32604b95a35e59d621b9d477aa6b24252327396a
jg20228 <jg20228@nate.com>
2020-03-20 13:12:19 +0900

    200320-4

--- src/ch03/MethodEx01.java
+++ src/ch03/MethodEx01.java
@@ -4,36 +4,17 @@ package ch03;
 //메서드(함수)는 메서드안에 작성할 수 없다.
 public class MethodEx01 {
 
-	static int add(int n1, int n2) {
-		int result = n1 + n2;
-		// System.out.println("result : " + result);
-		return result; // return 가장 마지막에!!
-	}
-	//minus(매개변수는 int형 두개) (return은 int형)
-	static int minus(int n1, int n2) {
-		int result = n1 - n2;
-		return result;
-	}
-	static int multi(int n1, int n2) {
-		int result = n1 * n2;
-		return result;
-	}
-	static int divide(int n1, int n2) {
-		int result = n1 / n2;
-		return result;
-	}
-
 	public static void main(String[] args) {
-		int num = MethodEx01.add(5, 3);
+		int num = Cal.add(5, 3);
 		System.out.println("결과 : " + num);
 
-		num = MethodEx01.minus(5, 3);
+		num = Cal.minus(5, 3);
 		System.out.println("결과 : " + num);
 		
-		num = MethodEx01.multi(5, 3);
+		num = Cal.multi(5, 3);
 		System.out.println("결과 : " + num);
 		
-		num = MethodEx01.divide(5, 3);
+		num = Cal.divide(5, 3);
 		System.out.println("결과 : " + num);
 	}
 
